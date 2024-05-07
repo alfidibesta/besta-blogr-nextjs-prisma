ec4d609575a4155d57087f36f8224ec01abbb482
besta <alfidibesta@gmail.com>
2024-05-08 01:03:46 +0700

    test

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -7,27 +7,25 @@ import prisma from "../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req });
-  if (!session) {
-    res.statusCode = 403;
-    return { props: { drafts: [] } };
-  }
-
-  const drafts = await prisma.post.findMany({
-    where: {
-      author: { email: session.user.email },
-      published: false,
-    },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  });
+  const drafts = session 
+    ? await prisma.post.findMany({
+        where: {
+          author: { email: session.user.email },
+          published: false,
+        },
+        include: {
+          author: {
+            select: { name: true },
+          },
+        },
+      })
+    : [];
 
   return {
     props: { drafts },
   };
 };
+
 
 type Props = {
   drafts: PostProps[];
